d384507db2b3a40164336d29d5efaba94e27e209
LevUE4 <l.nickitin2015@yandex.ru>
2022-01-27 08:22:54 +0500

    contour

---
+++ Source/Dental_Robot_DEMO/Private/BP_Jaw.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_Jaw.h"
+
+// Sets default values
+ABP_Jaw::ABP_Jaw()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABP_Jaw::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABP_Jaw::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
